da8730057a74b30b7b1e860a776d1113ffa79f10
Wangga Rahestu <62321623+wanggarahestu@users.noreply.github.com>
2020-04-25 01:48:20 +0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -4,6 +4,9 @@ public class InputOutput{
  public static void main(String []args){
   Scanner scanner = new Scanner(System.in);
   String nama;
+  int jumlahSKS;
+  double IPK;
+
   System.out.print("Masukkan nama anda");
   nama = scanner.nextLine();
   System.out.println("Hai, "+nama);
